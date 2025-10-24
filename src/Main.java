d955305e7a80cf98cf7ae4033dce3f3549361929
irfanhamid472 <irfanhamid2709@gmail.com>
2025-10-24 16:29:10 +0700

    apapun

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        System.out.print("Masukkan jumlah angka: ");
+        int n = scanner.nextInt();
+
+        int[] angka = new int[n];
+        System.out.println("Masukkan angka-angka:");
+
+        for (int i = 0; i < n; i++) {
+            angka[i] = scanner.nextInt();
         }
+
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total += angka[i];
+        }
+
+        double rataRata = (double) total / n;
+        System.out.println("Rata-rata adalah: " + rataRata);
+
+        scanner.close();
     }
+
+
 }
